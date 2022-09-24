eb8af4e857fa347bf4832335aed8dea9f1836e31
Carlos <carlos.av.amorim@gmail.com>
2022-09-24 18:06:34 -0300

    Feat: Creating SignIn page and Input component

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,32 @@
+import {
+  FormControl,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+//...rest coleta todas as outras props adicionadas no componente , pois o type não será fixo, devemos tipar com extends
+export default function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}:</FormLabel>}
+      <ChakraInput
+        name={name}
+        id={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: "gray.900",
+        }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
+import { Button, Flex, Stack } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import Input from "../components/Form/Input";
 
-const Home: NextPage = () => {
+const SignIn: NextPage = () => {
   return (
-    <>
-      <h1>DashGo</h1>
-    </>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <Input name="email" label="Email" type="email" />
+          <Input name="password" label="Senha" type="password" />
+          <Button type="submit" mt={6} colorScheme="pink" size="lg">
+            Entrar
+          </Button>
+        </Stack>
+      </Flex>
+    </Flex>
   );
 };
 
-export default Home;
+export default SignIn;
