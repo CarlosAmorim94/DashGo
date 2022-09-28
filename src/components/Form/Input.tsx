ac7a62df44989f5e67dc552e4f9a5e08ecd27d97
Carlos <carlos.av.amorim@gmail.com>
2022-09-28 17:14:50 -0300

    Feat: Add Reavh-hook-form and yup

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,19 +1,26 @@
 import {
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input as ChakraInput,
   InputProps as ChakraInputProps,
 } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
+import { FieldError } from "react-hook-form/dist/types";
 
 interface InputProps extends ChakraInputProps {
   name: string;
   label?: string;
+  error?: FieldError;
 }
 
 //...rest coleta todas as outras props adicionadas no componente , como o type não será fixo, devemos tipar com extends
-export default function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error = null, ...rest },
+  ref
+) => {
   return (
-    <FormControl>
+    <FormControl isInvalid={!!error}>
       {!!label && <FormLabel htmlFor={name}>{label}:</FormLabel>}
       <ChakraInput
         name={name}
@@ -26,7 +33,11 @@ export default function Input({ name, label, ...rest }: InputProps) {
         }}
         size="lg"
         {...rest}
+        ref={ref}
       />
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
   );
-}
+};
+
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,34 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import Input from "../components/Form/Input";
+import { Input } from "../components/Form/Input";
+import { useForm } from "react-hook-form";
+import { SubmitHandler } from "react-hook-form/dist/types";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const singInFormSchema = yup.object().shape({
+  email: yup.string().email("E-mail inválido").required("E-mail obrigatório"),
+  password: yup.string().required("Senha obrigatória"),
+});
 
 const SignIn: NextPage = () => {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(singInFormSchema),
+  });
+
+  const { errors } = formState;
+
+  //Tipamos a função com SubmitHandler(+generic de SignInFormData para values), pois já vem com o event tipado, caso precisamos usar, mas pode ser adicionado diretamente na props sem problems (values: SignInFormData)
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -13,11 +39,30 @@ const SignIn: NextPage = () => {
         p={8}
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing={4}>
-          <Input name="email" label="Email" type="email" />
-          <Input name="password" label="Senha" type="password" />
-          <Button type="submit" mt={6} colorScheme="pink" size="lg">
+          <Input
+            name="email"
+            label="Email"
+            type="email"
+            {...register("email")}
+            error={errors.email}
+          />
+          <Input
+            name="password"
+            label="Senha"
+            type="password"
+            {...register("password")}
+            error={errors.password}
+          />
+          <Button
+            type="submit"
+            mt={6}
+            colorScheme="pink"
+            size="lg"
+            isLoading={formState.isSubmitting}
+          >
             Entrar
           </Button>
         </Stack>
