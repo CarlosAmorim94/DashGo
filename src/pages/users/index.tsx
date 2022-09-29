30c2031aba1ea41bbaa4ae7d368d5e6dab6fd5a8
Carlos <carlos.av.amorim@gmail.com>
2022-09-29 16:31:09 -0300

    Feat: configuring MirageJs and React-Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,20 +13,30 @@ import {
   Tr,
   Text,
   useBreakpointValue,
+  Spinner,
 } from "@chakra-ui/react";
 import Link from "next/link";
-import React from "react";
 import { RiAddLine } from "react-icons/ri";
+import { useQuery } from "react-query";
+
 import Header from "../../components/Header";
 
 import Pagination from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery("users", async () => {
+    const response = await fetch("https://localhost:3000/api/users");
+    const data = await response.json();
+
+    return data;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
   return (
     <Box>
       <Header />
@@ -49,35 +59,49 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Carlos Amorim</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      carlos.av.amorim@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>27/09/2022</Td>}
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>
+                Falha ao obter dados dos usuários, recarregue a página
+              </Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Carlos Amorim</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          carlos.av.amorim@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>27/09/2022</Td>}
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
---
+++ src/services/mirage/index.ts
@@ -0,0 +1,48 @@
+import { createServer, Factory, Model } from "miragejs";
+import { faker } from "@faker-js/faker";
+
+type User = {
+  name: string;
+  email: string;
+  created_at: string;
+};
+
+export const makeServer = () => {
+  const server = createServer({
+    models: {
+      user: Model.extend<Partial<User>>({}),
+    },
+
+    //factories e seeds vão criar 200 usuários aleatórios utilizando a lib faker
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return `User ${i + 1}`;
+        },
+        email() {
+          return faker.internet.email().toLocaleLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(30);
+        },
+      }),
+    },
+
+    seeds(server) {
+      server.createList("user", 200);
+    },
+
+    routes() {
+      this.namespace = "api";
+      this.timing = 750;
+
+      this.get("/users");
+      this.post("/users");
+
+      this.namespace = "";
+      this.passthrough();
+    },
+  });
+
+  return server;
+};
